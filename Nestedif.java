a25de8709ca78bca4d4de24d40dd15ebf2562a49
adarshht7 <adarshht7@gmail.com>
2026-03-26 12:29:02 +0530

    Marks  program addaed

--- Nestedif.java
+++ Nestedif.java
@@ -3,12 +3,12 @@ public class Nestedif {
         int age=65;
         if(age>=18){
             if(age>=65){
-                System.out.println("Eligible to avail government fund");
-            }
-            
-        }   else  
-                 System.out.println("Not eligible to vote");
-           
+                System.out.println("Eligible to vote and avail government fund");
+            }else
+                System.out.println("we are eligible to vote but not eligible to avail government");
+
+        } else
+                 System.out.println("we are not eligiblle to vote and not eligible to avail government"); 
     
 }
 }
